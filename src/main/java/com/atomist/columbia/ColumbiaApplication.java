276118777bc845b6e79850e09f2611963aeba09e
Rod Johnson <johnsonroda@gmail.com>
2018-02-14 06:06:28 +0000

    Initial commit from Atomist

---
+++ src/main/java/com/atomist/columbia/ColumbiaApplication.java
@@ -0,0 +1,12 @@
+package com.atomist.columbia;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ColumbiaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ColumbiaApplication.class, args);
+	}
+}
